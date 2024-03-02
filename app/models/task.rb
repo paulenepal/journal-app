e6ec925d1fb0b01f29f7710be04d234a589c6844
Paulene Pallan <paulenepallan@gmail.com>
2024-03-02 22:13:30 +0800

    added validations (working) and scope (wip)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :task_category
+
+  scope :due_today, -> { where(due_date: Date.today) }
+  scope :completed, -> { where(completed: true) }
+
+  validates :name, presence: true
+  validates :description, length: { maximum: 200 }
 end
