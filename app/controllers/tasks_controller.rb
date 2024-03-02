e760b287bd7972f5205a574a253c3dc572e80482
Paulene Pallan <paulenepallan@gmail.com>
2024-03-02 22:11:26 +0800

    implemented destroy action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task_category, only: [:new, :create, :destroy]
+  before_action :set_task_category, only: [:new, :create]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def show
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
     @task.task_category = @task_category
 
     if @task.save
-      redirect_to task_categories_path
+      redirect_to task_category_path(@task_category)
     else
       render :new
     end
@@ -26,13 +26,15 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to task_categories_path
+      redirect_to task_category_path(@task.task_category)
     else
       render :edit
     end
   end
 
   def destroy
+    @task.destroy
+    redirect_to task_category_path(@task.task_category), notice: "Awesome! You've made a decisive move in boosting productivity by successfully deleting a task! 🤟"
   end
 
   private
@@ -47,6 +49,12 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def authorize_user(task)
+    unless task.user == current_user
+      redirect_to task_categories_path, alert: "You don't have permission to access this task category."
+    end
   end
 
 end
