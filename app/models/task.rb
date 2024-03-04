1ea3969fa43507c7243083a2d6078e58324a4d9a
Paulene Pallan <paulenepallan@gmail.com>
2024-03-04 23:07:43 +0800

    added scopes for today, overdue, completed, and upcoming tasks filters

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :task_category
 
-  scope :due_today, -> { where(due_date: Date.today) }
+  scope :today, -> { where("due_date >= ? AND due_date < ? AND completed = ?", Date.current, Date.current + 1, false).order(due_date: :asc) }
+  scope :overdue, -> { where("due_date < ? AND completed = ?", Date.current, false).order(due_date: :asc) }
   scope :completed, -> { where(completed: true) }
+  scope :upcoming, -> {where(due_date: Date.current..(Date.current + 2.weeks), completed: false).order(due_date: :asc)} # current date + 2 weeks
 
   validates :name, presence: true
   validates :description, length: { maximum: 200 }
