809817dd5bf49d3671e5e84bb8cc19d9497e7f68
Paulene Pallan <paulenepallan@gmail.com>
2024-03-05 15:12:27 +0800

    minor change: added a comment for the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :task_categories
-  has_many :tasks
+  has_many :tasks # this is needed when fetching the user's tasks / for task filtering
 end
