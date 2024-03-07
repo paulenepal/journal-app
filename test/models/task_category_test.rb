9e8a2f945ca8f7185c4cf5d0b996290c9ec92c30
Paulene Pallan <paulenepallan@gmail.com>
2024-03-07 22:43:55 +0800

    added task_category model test: should not save category if name is left blank or if name is > 20 char

--- test/models/task_category_test.rb
+++ test/models/task_category_test.rb
@@ -4,4 +4,15 @@ class TaskCategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save TaskCategory without name" do
+    task_category = TaskCategory.new
+    assert_not task_category.save, "Saved the TaskCategory without name"
+  end
+
+  test "should not save TaskCategory if name length > 20" do
+    task_category = TaskCategory.new
+    name = "This should not save. I am more than 20 characters."
+    assert_not task_category.save, "Saved the TaskCategory without name"
+  end
+
 end
