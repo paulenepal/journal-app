cf6d2b2d2e7fbbc0c4b2c3882a18bf5945d085ca
Paulene Pallan <paulenepallan@gmail.com>
2024-03-08 23:40:24 +0800

    updated  migration order

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,8 +6,7 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate VERSION=20240223104938
-bundle exec rails db:migrate VERSION=20240223110247
 bundle exec rails db:migrate VERSION=20240223110728
-bundle exec rails db:migrate VERSION=20240223114443
+bundle exec rails db:migrate VERSION=20240223110247
 bundle exec rails db:migrate VERSION=20240223110846
-bundle exec rails db:migrate VERSION=20240224020332
+bundle exec rails db:migrate VERSION=20240223114443
