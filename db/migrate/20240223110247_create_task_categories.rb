4b4e6690b388aa6b11723a3d801ad2c166e80abe
Paulene Pallan <paulenepallan@gmail.com>
2024-03-09 08:39:58 +0800

    updated  migration files: should fix deployment error in render

--- db/migrate/20240223110247_create_task_categories.rb
+++ db/migrate/20240223110247_create_task_categories.rb
@@ -2,7 +2,7 @@ class CreateTaskCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :task_categories do |t|
       t.text :name
-      t.references :task, null: false, foreign_key: { to_table: :tasks }
+      #t.references :task, null: false, foreign_key: { to_table: :tasks }
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
