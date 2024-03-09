1bf97e3deda8df8eafb6c7da032370405b5e1ddc
Paulene Pallan <paulenepallan@gmail.com>
2024-03-09 10:13:13 +0800

    updated  migration files: should fix deployment error in render

--- db/migrate/20240223124154_remove_task_from_task_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTaskFromTaskCategories < ActiveRecord::Migration[7.1]
-  def change
-    remove_reference :task_categories, :task, null: false, foreign_key: { to_table: :tasks }
-  end
-end
