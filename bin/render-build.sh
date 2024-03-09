b9c4228b8076d6977ec120032b09a4bc9b02ce3c
Paulene Pallan <paulenepallan@gmail.com>
2024-03-09 08:31:06 +0800

    updated  migration files: should fix deployment error in render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,4 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate VERSION=20240223104938
-bundle exec rails db:migrate VERSION=20240223110247
-bundle exec rails db:migrate VERSION=20240223110728
-bundle exec rails db:migrate VERSION=20240224020332
-bundle exec rails db:migrate VERSION=20240223110846
-bundle exec rails db:migrate VERSION=20240223114443
+bundle exec rails db:migrate
